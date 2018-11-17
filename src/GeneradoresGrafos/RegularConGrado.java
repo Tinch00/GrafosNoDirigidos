b64a8221931d804ab2ec2e357840fb798991614b
PMC <pablocruz90@gmail.com>
2018-11-16 21:10:37 -0300

    modificacion RegularConGrado

---
+++ src/GeneradoresGrafos/RegularConGrado.java
@@ -0,0 +1,27 @@
+package GeneradoresGrafos;
+
+public class RegularConGrado extends Generadora {
+	
+	private int grado;
+	
+	 public RegularConGrado(int cantNodos, int grado) {
+		super(cantNodos);
+		this.grado = grado;
+	}
+
+	@Override
+	public void generar() { 
+		if((this.matriz.getCantNodos() %2 == 1 && this.grado %2 ==1) || this.grado >= this.matriz.getCantNodos() || this.matriz.getCantNodos() == 1 || this.grado <=0) {
+			System.out.println("ERROR: No se puede generar un grafo de " + this.matriz.getCantNodos() + "nodos y grado " + this.grado);
+			return;
+		}
+			
+		for(int F = 0; F < this.matriz.getCantNodos(); F++) {
+			for(int C = 1; C <= this.grado/2; C++) {
+				this.matriz.setNodo(F, (F+C) % this.matriz.getCantNodos());
+			}
+			if(F < (this.matriz.getCantNodos()/2) && this.grado%2 == 1)
+				this.matriz.setNodo(F, F + (this.matriz.getCantNodos()/2));
+		}
+	}
+}
