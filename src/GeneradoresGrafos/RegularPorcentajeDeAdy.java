f3abe962354c9f15e701a693ddd9c0f1508ed876
Micaela <micaelapalau10@gmail.com>
2018-11-19 15:03:55 -0300

    Update RegularPorcentajeDeAdy.java

--- src/GeneradoresGrafos/RegularPorcentajeDeAdy.java
+++ src/GeneradoresGrafos/RegularPorcentajeDeAdy.java
@@ -1,5 +1,7 @@
 package GeneradoresGrafos;
 
+import java.io.IOException;
+
 public class RegularPorcentajeDeAdy extends Generadora {
 	
 	private double adyacencia;
@@ -12,6 +14,8 @@ public class RegularPorcentajeDeAdy extends Generadora {
 		@Override
 		public void generar() {
 			int grado = (int) ((this.adyacencia/100) * (this.matriz.getCantNodos()-1));
+			int gradoMaximo = 0;
+			int gradoMinimo = this.matriz.getCantNodos() - 1;
 			
 			
 			if((this.matriz.getCantNodos() %2 == 1 && grado %2 ==1) || grado >= this.matriz.getCantNodos() || this.matriz.getCantNodos() == 1 || grado <=0) {
@@ -25,6 +29,37 @@ public class RegularPorcentajeDeAdy extends Generadora {
 				}
 				if(F < (this.matriz.getCantNodos()/2) && grado%2 == 1)
 					this.matriz.setArista(F, F + (this.matriz.getCantNodos()/2));
+			}
+			
+			for (int indiceNodo = 0; indiceNodo < this.matriz.getCantNodos(); indiceNodo++) {
+				int gradoNodo = 0;
+				for (int C = 0; C < this.matriz.getCantNodos(); C++) {
+					if (indiceNodo != C) {
+						if (this.matriz.getArista(indiceNodo, C) == '1'){
+							gradoNodo++;
+						}
+					}
+				}
+				if (gradoNodo > gradoMaximo)
+					gradoMaximo = gradoNodo;
+				if (gradoNodo < gradoMinimo)
+					gradoMinimo = gradoNodo;
+			}
+			
+			try {
+				int cantAristas = this.matriz.getCantAristas();
+				int totalAristas = this.matriz.getTotalAristas();
+				double ady =((double)cantAristas/totalAristas)*100;
+				escribirGrafoEnArchivo(this.matriz.getCantNodos(), 
+									   this.matriz.getCantAristas(),
+									   ady,
+									   gradoMaximo,
+									   gradoMinimo,
+									   this.matriz.getListaAristas(),
+									   "bin/ArchivosEntrada/RegularConPorcentajeAdy"  + this.matriz.getCantNodos() + (this.adyacencia*100) + ".in");
+			} catch (IOException e) {
+				System.out.println("No se pudo guardar el archivo");
+				e.printStackTrace();
 			}
 			
 		}
